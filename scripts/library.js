cc7e3dd2b92c7ad6a5bfde8228e671736334f4d9
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-04-05 14:15:33 +0100

    add some more solutions and a automatic list generation with js

--- scripts/library.js
+++ scripts/library.js
@@ -1,18 +1,77 @@
+let languages = {
+	jv: "Java",
+	kt: "Kotlin",
+	rb: "Ruby",
+	c: "C"
+}
+
 let viewElements = [
 	{
 		"id": "kt",
-		"Name": "Kotlin",
 		"elementObj": null
 	},
 	{
 		"id": "rb",
-		"Name": "Ruby",
 		"elementObj": null
 	},
 	{
 		"id": "c",
-		"Name": "C",
 		"elementObj": null
+	},
+	{
+		"id": "jv",
+		"elementObj": null
+	}
+]
+
+let solutions = [
+	{
+		"problemName": "Breaking the Records",
+		"platform": "Hacker Rank",
+		"languages": ["jv", "kt", "rb"],
+		"descriptionLink": "https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem"
+	},
+	{
+		"problemName": "Cats and Mouse",
+		"platform": "Hacker Rank",
+		"languages": ["kt"],
+		"descriptionLink": "https://www.hackerrank.com/challenges/cats-and-a-mouse/problem"
+	},
+	{
+		"problemName": "Chocolate Feast",
+		"platform": "Hacker Rank",
+		"languages": ["jv", "kt", "rb"],
+		"descriptionLink": "https://www.hackerrank.com/challenges/chocolate-feast/problem"
+	},
+	{
+		"problemName": "Lattice Paths",
+		"platform": "Project Euler",
+		"languages": ["c"],
+		"descriptionLink": "https://projecteuler.net/problem=15"
+	},
+	{
+		"problemName": "New Year Chaos",
+		"platform": "Hacker Rank",
+		"languages": ["kt"],
+		"descriptionLink": "https://www.hackerrank.com/challenges/new-year-chaos/problem"
+	},
+	{
+		"problemName": "Ransom Note",
+		"platform": "Hacker Rank",
+		"languages": ["rb"],
+		"descriptionLink": "https://www.hackerrank.com/challenges/ctci-ransom-note/problem"
+	},
+	{
+		"problemName": "The Hurdle Race",
+		"platform": "Hacker Rank",
+		"languages": ["kt"],
+		"descriptionLink": "https://www.hackerrank.com/challenges/the-hurdle-race/problem"
+	},
+	{
+		"problemName": "Two Strings",
+		"platform": "Hacker Rank",
+		"languages": ["kt"],
+		"descriptionLink": "https://www.hackerrank.com/challenges/two-strings/problem"
 	}
 ]
 
@@ -46,6 +105,47 @@ function filter(){
 	}
 }
 
+function generateSolutionsList(){
+	let unorderedList = document.getElementById("solutions-list");
+
+	//solution
+	for(var i = 0; i < solutions.length; i++){
+		//language
+		for(var j = 0; j < solutions[i].languages.length; j++){
+
+			var li = document.createElement("li");
+			li.classList.add("list-group-item");
+			li.classList.add(solutions[i].languages[j] + "c");
+
+			var icon = document.createElement("i");
+			icon.classList.add("devicon-" + languages[solutions[i].languages[j]].toLowerCase() + "-plain");
+
+			var span =  document.createElement("span");
+			span.classList.add("list-icon-padding");
+			span.insertAdjacentHTML('afterbegin', solutions[i].problemName + " - " + solutions[i].platform)
+
+			var linkCode = document.createElement("a");
+			linkCode.classList.add("paddingl20");
+			linkCode.setAttribute('target', '_blank');
+			linkCode.setAttribute('href', 'codelibrary/' + solutions[i].problemName.toLowerCase().replaceAll(" ", "") + solutions[i].languages[j] + ".html")
+			linkCode.insertAdjacentHTML('afterbegin',"Code");
+
+			var linkDescription = document.createElement("a");
+			linkDescription.classList.add("paddingl20");
+			linkDescription.setAttribute('target', '_blank');
+			linkDescription.setAttribute('href',solutions[i].descriptionLink)
+			linkDescription.insertAdjacentHTML('afterbegin',"Description");
+
+			li.appendChild(icon);
+			li.appendChild(span);
+			li.appendChild(linkCode);
+			li.appendChild(linkDescription);
+
+			unorderedList.appendChild(li);
+		}
+	}
+}
+
 function listener(e){
 	filter()
 }
@@ -54,8 +154,11 @@ function listener(e){
 	viewElements[0].elementObj = document.getElementById("kt");
 	viewElements[1].elementObj = document.getElementById("rb");
 	viewElements[2].elementObj = document.getElementById("c");
+	viewElements[3].elementObj = document.getElementById("jv");
 
 	for(var i = 0; i < viewElements.length; i++){
 		viewElements[i].elementObj.addEventListener('change', listener)
 	}
+
+	generateSolutionsList()
 })();
