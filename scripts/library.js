3552f1eb8b17088fef81f23359b2467643c9b8f0
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-04-06 13:58:47 +0100

    restyling done

--- scripts/library.js
+++ scripts/library.js
@@ -5,171 +5,141 @@ let languages = {
 	c: "C"
 }
 
-let viewElements = [
-	{
-		"id": "kt",
-		"elementObj": null
-	},
-	{
-		"id": "rb",
-		"elementObj": null
-	},
-	{
-		"id": "c",
-		"elementObj": null
-	},
-	{
-		"id": "jv",
-		"elementObj": null
-	}
-]
-
 let solutions = [
 	{
 		"problemName": "Breaking the Records",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem",
+		"problemDescription": "Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there. Given the scores for a season, determine the number of times Maria breaks her records for most and least points scored during the season."
 	},
 	{
 		"problemName": "Cats and Mouse",
 		"platform": "Hacker Rank",
 		"languages": ["kt"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/cats-and-a-mouse/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/cats-and-a-mouse/problem",
+		problemDescription: "Two cats and a mouse are at various positions on a line. You will be given their starting positions. Your task is to determine which cat will reach the mouse first, assuming the mouse does not move and the cats travel at equal speed. If the cats arrive at the same time, the mouse will be allowed to move and it will escape while they fight."
 	},
 	{
 		"problemName": "Chocolate Feast",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/chocolate-feast/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/chocolate-feast/problem",
+		"problemDescription": "Little Bobby loves chocolate. He frequently goes to his favorite 5&10 store, Penny Auntie, to buy them. They are having a promotion at Penny Auntie. If Bobby saves enough wrappers, he can turn them in for a free chocolate."
 	},
 	{
 		"problemName": "Lattice Paths",
 		"platform": "Project Euler",
 		"languages": ["c"],
-		"descriptionLink": "https://projecteuler.net/problem=15"
+		"descriptionLink": "https://projecteuler.net/problem=15",
+		"problemDescription": "Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner. How many such routes are there through a 20×20 grid?"
 	},
 	{
 		"problemName": "New Year Chaos",
 		"platform": "Hacker Rank",
 		"languages": ["kt"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/new-year-chaos/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/new-year-chaos/problem",
+		"problemDescription": "It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from 1 to n. Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others. Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic."
 	},
 	{
 		"problemName": "Ransom Note",
 		"platform": "Hacker Rank",
 		"languages": ["rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/ctci-ransom-note/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/ctci-ransom-note/problem",
+		"problemDescription": "Harold is a kidnapper who wrote a ransom note, but now he is worried it will be traced back to him through his handwriting. He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use only whole words available in the magazine. He cannot use substrings or concatenation to create the words he needs. Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No."
 	},
 	{
 		"problemName": "The Hurdle Race",
 		"platform": "Hacker Rank",
 		"languages": ["kt"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/the-hurdle-race/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/the-hurdle-race/problem",
+		problemDescription: "A video player plays a game in which the character competes in a hurdle race. Hurdles are of varying heights, and the characters have a maximum height they can jump. There is a magic potion they can take that will increase their maximum jump height by 1 unit for each dose. How many doses of the potion must the character take to be able to jump all of the hurdles. If the character can already clear all of the hurdles, return 0."
 	},
 	{
 		"problemName": "Two Strings",
 		"platform": "Hacker Rank",
 		"languages": ["kt"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/two-strings/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/two-strings/problem",
+		"problemDescription": "Given two strings, determine if they share a common substring. A substring may be as small as one character."
 	},
 	{
 		"problemName": "Equalize the Array",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/equality-in-a-array/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/equality-in-a-array/problem",
+		"problemDescription": "Given an array of integers, determine the minimum number of elements to delete to leave only elements of equal value."
 	},
 	{
 		"problemName": "Minimum Swipes 2",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/minimum-swaps-2/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/minimum-swaps-2/problem",
+		"problemDescription": "You are given an unordered array consisting of consecutive integers \u2208 [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. Find the minimum number of swaps required to sort the array in ascending order."
 	},
 	{
 		"problemName": "Counting Valleys",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/counting-valleys/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/counting-valleys/problem",
+		"problemDescription": "Given the sequence of up and down steps during a hike, find and print the number of valleys walked through."
 	},
 	{
 		"problemName": "Drawing Book",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/drawing-book/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/drawing-book/problem",
+		"problemDescription": "A teacher asks the class to open their books to a page number. A student can either start turning pages from the front of the book or from the back of the book. They always turn pages one at a time. When they open the book, page 1 is always on the right side. When they flip page 1, they see pages 2 and 3. Each page except the last page will always be printed on both sides. The last page may only be printed on the front, given the length of the book. If the book is n pages long, and a student wants to turn to page p, what is the minimum number of pages to turn? They can start at the beginning or the end of the book. Given n and p, find and print the minimum number of pages that must be turned in order to arrive at page p."
 	},
 	{
 		"problemName": "The Time in Words",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/the-time-in-words/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/the-time-in-words/problem",
+		"problemDescription": "Write a program which prints the time in words for the input given in the format described."
 	},
 	{
 		"problemName": "Halloween Sale",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/halloween-sale/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/halloween-sale/problem",
+		"problemDescription": "You wish to buy video games from the famous online video game store Mist. Usually, all games are sold at the same price, p dollars. However, they are planning to have the seasonal Halloween Sale next month in which you can buy games at a cheaper price. Specifically, the first game will cost p dollars, and every subsequent game will cost d dollars less than the previous one. This continues until the cost becomes less than or equal to m dollars, after which every game will cost m dollars. How many games can you buy during the Halloween Sale?"
 	},
 	{
 		"problemName": "Minimum Distances",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/minimum-distances/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/minimum-distances/problem",
+		"problemDescription": "The distance between two array values is the number of indices between them. Given a, find the minimum distance between any pair of equal elements in the array. If no such value exists, return -1."
 	},
 	{
 		"problemName": "Repeated String",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/repeated-string/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/repeated-string/problem",
+		"problemDescription": "There is a string, s, of lowercase English letters that is repeated infinitely many times. Given an integer, n, find and print the number of letter a's in the first n letters of the infinite string."
 	},
 	{
 		"problemName": "Day of the Programmer",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/day-of-the-programmer/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/day-of-the-programmer/problem",
+		"problemDescription": "Given a year, y, find the date of the 256th day of that year according to the official Russian calendar during that year. Then print it in the format dd.mm.yyyy, where dd is the two-digit day, mm is the two-digit month, and yyyy is y."
 	},
 	{
 		"problemName": "Migratory Birds",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/migratory-birds/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/migratory-birds/problem",
+		"problemDescription": "Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type. If more than 1 type has been spotted that maximum amount, return the smallest of their ids."
 	},
 	{
 		"problemName": "Subarray Division",
 		"platform": "Hacker Rank",
 		"languages": ["jv", "kt", "rb"],
-		"descriptionLink": "https://www.hackerrank.com/challenges/the-birthday-bar/problem"
+		"descriptionLink": "https://www.hackerrank.com/challenges/the-birthday-bar/problem",
+		"problemDescription": "Two children, Lily and Ron, want to share a chocolate bar. Each of the squares has an integer on it. Lily decides to share a contiguous segment of the bar selected such that: The length of the segment matches Ron's birth month, and, The sum of the integers on the squares is equal to his birth day. Determine how many ways she can divide the chocolate."
 	}
 ]
-
-function filter(){
-	var i;
-	var toHide = []
-	var toShow = []
-	var allUnchecked = true
-
-	for(i = 0; i < viewElements.length; i++){
-		if(!viewElements[i].elementObj.checked){
-			let elements = document.getElementsByClassName(viewElements[i].id + 'c')
-			for(var j = 0; j < elements.length; j++){
-				toHide.push(elements[j]);
-			}
-		}else{
-			allUnchecked = false
-			let elements = document.getElementsByClassName(viewElements[i].id + 'c')
-			for(var j = 0; j < elements.length; j++){
-				toShow.push(elements[j]);
-			}
-		}
-	}
-
-	for(var i = 0; i < toHide.length; i++){
-		toHide[i].style.display = allUnchecked ? "" : "none"
-	}
-
-	for(var i = 0; i < toShow.length; i++){
-		toShow[i].style.display = ""
-	}
-}
 
 function solutionsCompare(sol1, sol2){
 	return sol1.problemName.localeCompare(sol2.problemName);
@@ -180,60 +150,92 @@ function generateSolutionsList(){
 
 	//solution
 	for(var i = 0; i < solutions.length; i++){
+
+		var cardDiv = document.createElement("div");
+		cardDiv.classList.add("card");
+
+		var rowDiv = document.createElement("div");
+		rowDiv.classList.add("row");
+
+		var colMd3 = document.createElement("div");
+		colMd3.classList.add("col-md-3");
+		colMd3.classList.add("bg-primary"); //HERE WE DEFINE THE BACKGROUND OF THE LEFT PART OF THE LIST ITEM
+
+		var solutionHeaderDiv = document.createElement("div");
+		solutionHeaderDiv.classList.add("card-body");
+		solutionHeaderDiv.classList.add("cc-problem-solution-header");
+
+		var platformP = document.createElement("p");
+		platformP.insertAdjacentHTML('afterbegin', solutions[i].platform);
+
+		var problemNameDiv = document.createElement("div");
+		problemNameDiv.classList.add("h5");
+		problemNameDiv.insertAdjacentHTML('afterbegin', solutions[i].problemName);
+
+		//Appending Children of Header div
+		solutionHeaderDiv.appendChild(platformP);
+		solutionHeaderDiv.appendChild(problemNameDiv);
+
+		var cardLeftHeaderSeparator = document.createElement("hr");
+
+		var languagesContainer = document.createElement("div");
+		languagesContainer.classList.add("languages-container-margins");
+		languagesContainer.classList.add("text-center")
+
 		//language
 		for(var j = 0; j < solutions[i].languages.length; j++){
 
-			var li = document.createElement("li");
-			li.classList.add("list-group-item");
-			li.classList.add(solutions[i].languages[j] + "c");
+			var linkCode = document.createElement("a");
+			linkCode.classList.add("language-icon");
+			linkCode.setAttribute('target', '_blank');
+			linkCode.setAttribute('title', languages[solutions[i].languages[j]]);
+			linkCode.setAttribute('href', 'codelibrary/' + solutions[i].problemName.toLowerCase().replaceAll(" ", "") + solutions[i].languages[j] + ".html")
 
 			var icon = document.createElement("i");
 			icon.classList.add("devicon-" + languages[solutions[i].languages[j]].toLowerCase() + "-plain");
-			icon.classList.add("colored")
 
-			var span =  document.createElement("span");
-			span.classList.add("list-icon-padding");
-			span.insertAdjacentHTML('afterbegin', solutions[i].problemName + " - " + solutions[i].platform)
+			linkCode.appendChild(icon);
 
-			var linkCode = document.createElement("a");
-			linkCode.classList.add("marginl20");
-			linkCode.classList.add("btn");
-			linkCode.classList.add("btn-outline-primary");
-			linkCode.setAttribute('target', '_blank');
-			linkCode.setAttribute('href', 'codelibrary/' + solutions[i].problemName.toLowerCase().replaceAll(" ", "") + solutions[i].languages[j] + ".html")
-			linkCode.insertAdjacentHTML('afterbegin',"Code");
+			languagesContainer.appendChild(linkCode);
+		}
 
-			var linkDescription = document.createElement("a");
-			linkDescription.classList.add("marginl20");
+		//APPENDING CHILDREN FOR THE FIRST COLUMN
+		colMd3.appendChild(solutionHeaderDiv);
+		colMd3.appendChild(cardLeftHeaderSeparator);
+		colMd3.appendChild(languagesContainer);
+
+		var colMd9 = document.createElement("div");
+		colMd9.classList.add("col-md-9");
+
+		var cardBodyDiv = document.createElement("div");
+		cardBodyDiv.classList.add("card-body");
+
+		var problemDescriptionP = document.createElement("p");
+		problemDescriptionP.insertAdjacentHTML('afterbegin',solutions[i].problemDescription) // change this later after insert the problem description to the array
+
+		var linkDescription = document.createElement("a");
 			linkDescription.classList.add("btn");
-			linkDescription.classList.add("btn-outline-primary");
+			linkDescription.classList.add("btn-primary");
 			linkDescription.setAttribute('target', '_blank');
 			linkDescription.setAttribute('href',solutions[i].descriptionLink)
-			linkDescription.insertAdjacentHTML('afterbegin',"Description");
+			linkDescription.insertAdjacentHTML('afterbegin',"More Details and Examples");
 
-			li.appendChild(icon);
-			li.appendChild(span);
-			li.appendChild(linkCode);
-			li.appendChild(linkDescription);
+		//appending children for cardBodyDiv
+		cardBodyDiv.appendChild(problemDescriptionP);
+		cardBodyDiv.appendChild(linkDescription);
 
-			unorderedList.appendChild(li);
-		}
+		colMd9.appendChild(cardBodyDiv);
+
+		rowDiv.appendChild(colMd3);
+		rowDiv.appendChild(colMd9);
+
+		cardDiv.appendChild(rowDiv);
+
+		unorderedList.appendChild(cardDiv);
 	}
-}
-
-function listener(e){
-	filter()
 }
 
 (function() {
-	viewElements[0].elementObj = document.getElementById("kt");
-	viewElements[1].elementObj = document.getElementById("rb");
-	viewElements[2].elementObj = document.getElementById("c");
-	viewElements[3].elementObj = document.getElementById("jv");
-
-	for(var i = 0; i < viewElements.length; i++){
-		viewElements[i].elementObj.addEventListener('change', listener)
-	}
 
 	solutions.sort(solutionsCompare);
 
