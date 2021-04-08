959036dad29f2650179b7bb32cc7309e3d3a401c
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-04-08 19:19:45 +0100

    change lattice paths description

--- scripts/library.js
+++ scripts/library.js
@@ -32,7 +32,7 @@ let solutions = [
 		"platform": "Project Euler",
 		"languages": ["c"],
 		"descriptionLink": "https://projecteuler.net/problem=15",
-		"problemDescription": "Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner. How many such routes are there through a 20×20 grid?"
+		"problemDescription": "Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner. How many such routes are there through a 20×20 grid? <br/><br/><b>Response: 137846528820</b>"
 	},
 	{
 		"problemName": "New Year Chaos",
