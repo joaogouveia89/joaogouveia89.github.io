bdbc739825222108e3d74860c78b283d98a5cfd6
Joao Gouveia <jvgouve@sibs.com>
2025-05-22 00:10:36 +0100

    updated description

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ const resumeData = {
   github: 'joaogouveia89',
   jobTitle: 'Software Engineer',
   stackToDisplay: ['android', 'scrum', 'git', 'mvvm', 'jetpack-compose', 'kotlin', 'java', 'ruby-on-rails'],
-  description: 'Software engineer with experience in native Android development using Kotlin and Java, along with a strong background in libraries and frameworks for the platform. I rely on a self-task list and a professional journal to track my activities, progress, and upcoming tasks. Jiu-jitsu is my favorite hobby, and I practice it three times a week, which has greatly contributed to my personal and professional growth.',
+  description: 'Software engineer with experience in native Android development using Kotlin and Java, along with a strong background in libraries and frameworks for the platform. Recently, I have been expanding my skill set into data science, data analysis, and machine learning, exploring new ways to solve problems with data-driven approaches. I rely on a self-task list and a professional journal to track my activities, progress, and upcoming tasks. Jiu-jitsu is my favorite hobby, and I practice it three times a week, which has greatly contributed to my personal and professional growth.',
   experiences: [
     {
       jobTitle: 'Android engineer',
